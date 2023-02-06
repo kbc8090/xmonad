2f72a5d9c79f1027338a823d6876d6cabbfe5df4
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2023-02-06 17:53:30 -0500

    Add files via upload

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -8,8 +8,8 @@ function run {
 }
 
 #xset r rate 300 60 &
-trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --height 20 --iconspacing 3 --margin 3 --transparent true --alpha 0 --tint 0x202229 --widthtype request &
 xmobar &
-xsetroot -cursor_name left_ptr &
 picom &
+trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --height 16 --distancefrom top --distance 2 --iconspacing 2 --margin 2 --transparent true --alpha 0 --tint 0x202229 --widthtype request &
+xsetroot -cursor_name left_ptr &
 #nitrogen --restore &
